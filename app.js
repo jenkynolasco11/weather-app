94336bf71922bee04429e872310e54c359f7ca05
Jenky W. Nolasco <jenky.nolasco@gmail.com>
2017-03-01 17:11:53 -0400

    added route and data response

--- app.js
+++ app.js
@@ -1,34 +1,59 @@
 import Koa from 'koa'
-import bodyparser from 'koa-bodyparser'
 import logger from 'koa-logger'
+import bodyparser from 'koa-bodyparser'
 import koaRouter from 'koa-router'
-import fetch from 'fetch'
+import fetch from 'node-fetch'
 
 import config from './config'
 
-const server = new Koa()
+const app = new Koa()
 
-const route = () => {
+const route = (() => {
   const router = koaRouter()
+  let temp = 'nothing'
+
+  router.prefix('/')
 
   router.get('/:key/:lon/:lat', async (ctx, next) => {
     try {
-      const temp = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${ ctx.params.lat }&lon=${ ctx.params.lon }&appid=${ config.APIKEY }`)
-      ctx.body = { temp }
+      if(ctx.params.key === config.KEY) {
+        temp = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${ ctx.params.lat }&lon=${ ctx.params.lon }&appid=${ config.APIKEY }`)
+        temp = await temp.json()
+      } else {
+        temp = 'request not found or key not matched'
+        ctx.body = temp
+
+        return
+      }
+
+      ctx.body = {
+        temp : {
+          coords : temp.coord,
+          weather : temp.weather,
+          id : temp.id,
+          name : temp.name,
+          code : temp.cod
+        }
+      }
     } catch (er) {
-      //
+      console.log(er)
     }
   })
 
-  return router
-}
+  router.get('/', async (ctx) => {
+    ctx.body = 'hola mundo'
+  })
 
-server
+  return router
+})()
+
+app
   .use(bodyparser())
   .use(logger())
   .use(route.routes())
-  .use(route.allowedRoutes())
+  .use(route.allowedMethods())
 
-server.listen(config.PORT, () => {
-  // console.log()
+app.listen(config.PORT, () => {
+  if(config.ENV === 'development')
+    console.log(`started server at ${ config.PORT }`)
 })
--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 const PORT = 8080
 const APIKEY = 'e9f5722b65facd670a6c992f6b4f6c5a'
+const KEY = 'react-native-secret'
+const ENV = 'development'
 
 export default {
   APIKEY,
-  PORT
+  PORT,
+  KEY,
+  ENV
 }
